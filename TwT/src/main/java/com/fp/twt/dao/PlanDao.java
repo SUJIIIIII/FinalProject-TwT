c7c44652ef764c40ae8789c531d34c86edf03e95
DoYoung93 <qed4430@naver.com>
2020-02-13 21:21:45 +0900

    start

---
+++ TwT/src/main/java/com/fp/twt/dao/PlanDao.java
@@ -0,0 +1,31 @@
+package com.fp.twt.dao;
+
+import java.util.List;
+
+import com.fp.twt.vo.CityVo;
+
+public interface PlanDao {
+	String NAMESPACE = "twt.";
+	//철환
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+	
+	//수지
+	public List<CityVo> selectCityAll(); //plan_create 페이지에서 city정보 전부 불러오기
+	
+	
+	
+	
+	
+	
+	
+
+}
