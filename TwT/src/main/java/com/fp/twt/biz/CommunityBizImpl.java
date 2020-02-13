24db2d9bb8d721f085dac78b7fcd8c68d8a484fe
suji <gkdmd19@hanmail.net>
2020-02-13 14:50:35 +0900

    dao,biz,vo 수정

---
+++ TwT/src/main/java/com/fp/twt/biz/CommunityBizImpl.java
@@ -0,0 +1,25 @@
+package com.fp.twt.biz;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunityBizImpl implements CommunityBiz{
+	//용훈
+	
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+	
+	//도영
+	
+	
+	
+	
+	
+	
+
+}
---
+++ TwT/src/main/java/com/fp/twt/dao/MypageDaoImpl.java
@@ -0,0 +1,8 @@
+package com.fp.twt.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MypageDaoImpl implements MypageDao{
+
+}
