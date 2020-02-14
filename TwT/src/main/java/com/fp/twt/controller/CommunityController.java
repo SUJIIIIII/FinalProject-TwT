ee2f3fa7c5971fbe2016b71b7a0af27568c615bd
DoYoung93 <qed4430@naver.com>
2020-02-14 13:41:14 +0900

    20.02.14 version01 도영

--- TwT/src/main/java/com/fp/twt/controller/CommunityController.java
+++ TwT/src/main/java/com/fp/twt/controller/CommunityController.java
@@ -2,6 +2,7 @@ package com.fp.twt.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,9 @@ import com.fp.twt.biz.CommunityBiz;
 public class CommunityController {
 	private static final Logger logger = LoggerFactory.getLogger(CommunityController.class);
 	
+	@Autowired
 	private CommunityBiz biz;
+	
 	//용훈
 	
 	
@@ -26,10 +29,10 @@ public class CommunityController {
 	// 리스트 뿌려주는
 	@RequestMapping("/community.do")
 	public String community(Model model) {
-		/*
-		 * logger.info("SELECT LIST"); model.addAttribute("community",
-		 * biz.TS_selectList());
-		 */
+		
+		 logger.info("SELECT LIST"); 
+		 model.addAttribute("community", biz.TS_selectList());
+		 System.out.println(model);
 		
 		return "TwTCommunity/community_list";
 	}
