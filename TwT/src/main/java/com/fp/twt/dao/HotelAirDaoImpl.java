658102b7439b4e12eca2e806cc09aa2feb13f8a6
LimYonghun <60770112+LimYonghun@users.noreply.github.com>
2020-02-14 13:32:20 +0900

    제발

---
+++ TwT/src/main/java/com/fp/twt/dao/HotelAirDaoImpl.java
@@ -0,0 +1,25 @@
+package com.fp.twt.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HotelAirDaoImpl implements HotelAirDao{
+	//찬우
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+	
+	//범식
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ TwT/src/main/java/com/fp/twt/vo/PagingVo.java
@@ -0,0 +1,5 @@
+package com.fp.twt.vo;
+
+public class PagingVo {
+
+}
