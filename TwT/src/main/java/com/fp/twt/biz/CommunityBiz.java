4fc8b45acb9b83fd9d831452f6b1a449b606629d
ch200203 <58754885+ch200203@users.noreply.github.com>
2020-02-14 14:46:31 +0900

    다시 커밋했어요
    다시 커밋했어요

---
+++ TwT/src/main/java/com/fp/twt/biz/CommunityBiz.java
@@ -0,0 +1,28 @@
+package com.fp.twt.biz;
+
+import java.util.List;
+
+import com.fp.twt.vo.TravelScheduleVo;
+
+public interface CommunityBiz {
+
+	//용훈
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+	
+	//도영
+	
+	public List<TravelScheduleVo> TS_selectList();	
+	
+	
+	
+	
+	
+	
+
+}
