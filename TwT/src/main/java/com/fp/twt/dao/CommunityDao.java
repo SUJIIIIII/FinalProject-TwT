9657937047a5832644095cadf8fb1a0a589ae618
cwk1301 <cwk1301@gmail.com>
2020-02-14 14:24:27 +0900

    찬우

---
+++ TwT/src/main/java/com/fp/twt/dao/CommunityDao.java
@@ -0,0 +1,28 @@
+package com.fp.twt.dao;
+
+import java.util.List;
+
+import com.fp.twt.vo.TravelScheduleVo;
+
+public interface CommunityDao {
+	String NAMESPACE = "twt.";
+	//용훈
+	
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+	
+	//도영
+	
+	List<TravelScheduleVo> TS_selectList();
+	
+	
+	
+	
+	
+
+}
