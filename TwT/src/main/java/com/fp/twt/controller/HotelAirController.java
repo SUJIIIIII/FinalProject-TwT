d68caf89030ce09ca899735d0c52b0c105fa5da5
cwk1301 <cwk1301@gmail.com>
2020-02-14 14:00:26 +0900

    찬우02/14

--- TwT/src/main/java/com/fp/twt/controller/HotelAirController.java
+++ TwT/src/main/java/com/fp/twt/controller/HotelAirController.java
@@ -1,12 +1,19 @@
 package com.fp.twt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HotelAirController {
 	//찬우
 	
 	
+	@RequestMapping("/airsearch.do")//항공권 검색 
+	public String airSearch() {
+		
+		
+		return "TwTFlight/flight_list";
+	}
 	
 	
 	
