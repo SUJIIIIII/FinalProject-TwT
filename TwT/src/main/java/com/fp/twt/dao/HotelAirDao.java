84862f272ab91fc051c833d9716e0b972d268c85
kin7347 <kin7347@gmail.com>
2020-02-19 13:44:27 +0900

    추가

---
+++ TwT/src/main/java/com/fp/twt/dao/HotelAirDao.java
@@ -0,0 +1,31 @@
+package com.fp.twt.dao;
+
+import java.util.List;
+
+import com.fp.twt.vo.HotelVo;
+import com.fp.twt.vo.HotelroomVo;
+
+public interface HotelAirDao {
+	String NAMESPACE = "twt.";
+	//찬우
+	
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------------------------------//
+		
+	//범식
+		
+	public List<HotelVo> HselectList();
+	public List<HotelroomVo> selectall_B();
+		
+		
+		
+		
+		
+
+
+}
