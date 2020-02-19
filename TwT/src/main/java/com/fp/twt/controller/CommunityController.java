3ce853d7dbd7eb5d7916512fe4b0dddaa74457a3
ch200203 <58754885+ch200203@users.noreply.github.com>
2020-02-19 12:40:25 +0900

    철환 02/19
    일정표 데이터 추가

--- TwT/src/main/java/com/fp/twt/controller/CommunityController.java
+++ TwT/src/main/java/com/fp/twt/controller/CommunityController.java
@@ -31,143 +31,134 @@ import com.fp.twt.vo.MemberVo;
 import com.fp.twt.vo.ScheduleReviewVo;
 import com.fp.twt.vo.TravelScheduleVo;
 
-
-
 @Controller
 public class CommunityController {
 	private static final Logger logger = LoggerFactory.getLogger(CommunityController.class);
-	
+
 	@Autowired
 	private CommunityBiz biz;
-	
-	//용훈
-	
-	//포토북 이미지 업로드
-	@PostMapping(value="uploadImageFile.do", produces = "application/json")
+
+	// 용훈
+
+	// 포토북 이미지 업로드
+	@PostMapping(value = "uploadImageFile.do", produces = "application/json")
 	@ResponseBody
-	public Map<String, String> uploadSummernoteImageFile(HttpServletRequest request, @RequestParam("file") MultipartFile multipartFile) throws FileNotFoundException {
-		
+	public Map<String, String> uploadSummernoteImageFile(HttpServletRequest request,
+			@RequestParam("file") MultipartFile multipartFile) throws FileNotFoundException {
+
 		Map<String, String> map = new HashMap<String, String>();
-		
-		String fileRoot = "C:\\potoBook_image\\";  //저장될 외부 파일 경로
-		
-		String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
-		String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
-		
-		String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
-		
+
+		String fileRoot = "C:\\potoBook_image\\"; // 저장될 외부 파일 경로
+
+		String originalFileName = multipartFile.getOriginalFilename(); // 오리지날 파일명
+		String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 파일 확장자
+
+		String savedFileName = UUID.randomUUID() + extension; // 저장될 파일 명
+
 		File targetFile = new File(fileRoot + savedFileName);
-		
+
 		try {
 			InputStream fileStream = multipartFile.getInputStream();
-			FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+			FileUtils.copyInputStreamToFile(fileStream, targetFile); // 파일 저장
 			map.put("url", "/potoBook/" + savedFileName);
-			
+
 		} catch (IOException e) {
-			FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+			FileUtils.deleteQuietly(targetFile); // 저장된 파일 삭제
 			e.printStackTrace();
 		}
-		
+
 		return map;
 	}
-	
-	//포토북 insertForm
+
+	// 포토북 insertForm
 	@RequestMapping("communityInsertForm.do")
-	public String communityInsert(Model model, HttpServletRequest request){
+	public String communityInsert(Model model, HttpServletRequest request) {
 		int res = biz.insertForm();
-		
+
 		HttpSession session = request.getSession(false);
-		MemberVo member = (MemberVo)session.getAttribute("user");
-		
-		if(res > 0) {
-			String srCode = "SR"+biz.selectSrCode();
+		MemberVo member = (MemberVo) session.getAttribute("user");
+
+		if (res > 0) {
+			String srCode = "SR" + biz.selectSrCode();
 			model.addAttribute("sr_Code", srCode);
 			model.addAttribute("m_Code", member.getm_Code());
 		}
-		
+
 		return "TwTCommunity/community_insert";
 	}
-	
+
 	@RequestMapping("potoBookinsert.do")
 	public String communityUpdate(Model model, ScheduleReviewVo vo, HttpServletRequest request) throws IOException {
-		
-		String[] imgs = request.getParameterValues("potoImg"); //업로드된 사진 경로들
+
+		String[] imgs = request.getParameterValues("potoImg"); // 업로드된 사진 경로들
 		String content = vo.getSr_Content();
-		
+
 		List<String> existimgs = new ArrayList<String>();
-		
-		for(String src : imgs) {
-			if(content.contains(src)) {
+
+		for (String src : imgs) {
+			if (content.contains(src)) {
 				existimgs.add(src);
 			}
 		}
-		
-		for(String src : existimgs) {
-			String fileRoot = "C:\\potoBook_image\\"+vo.getSr_Code()+"\\";	
+
+		for (String src : existimgs) {
+			String fileRoot = "C:\\potoBook_image\\" + vo.getSr_Code() + "\\";
 			String extension = src.substring(src.lastIndexOf("."));
 			String savedFileName = UUID.randomUUID() + extension;
-			
+
 			File newFile = new File(fileRoot + savedFileName);
-			
+
 			byte[] bytes = FileCopyUtils.copyToByteArray(newFile);
-			
-			
-			
+
 		}
-		
-		
-		
-		
+
 		int res = biz.potoBookUpdate(vo);
-		
+
 		return "TwTCommunity/community_list";
 	}
-	
+
 	@RequestMapping("potoBookDetail.do")
 	public String potoBookDetail(Model model) {
-		
+
 		return "TwTCommunity/potoBook_detail";
 	}
-	//----------------------------------------------------------------------------------------------
-	
+	// ----------------------------------------------------------------------------------------------
+
 	// 도영
 	// 여행 일정 리스트
 	@RequestMapping("/community.do")
 	public String newcommunity(Model model) {
-		
-		logger.info("SELECT LIST"); 
+
+		logger.info("SELECT LIST");
 
 		List res = biz.selectList_D();
-		TravelScheduleVo vo = (TravelScheduleVo)res.get(0);
+		TravelScheduleVo vo = (TravelScheduleVo) res.get(0);
 		System.out.println("컨트롤러 id " + vo.getM_Id());
-		
+
 		model.addAttribute("community", biz.selectList_D());
 		System.out.println(model);
-		
+
 		return "TwTCommunity/community_list";
 	}
 
 	@RequestMapping("/popcommunity.do")
 	public String popcommunity(Model model) {
-		
-		logger.info("SELECT LIST"); 
+
+		logger.info("SELECT LIST");
 		model.addAttribute("community", biz.PselectList_D());
 		System.out.println(model);
-		
+
 		return "TwTCommunity/community_list";
 	}
-	
+
 	// 여행 일정 디테일
 	@RequestMapping("communityDetail.do")
-	public String communityDetail(Model model, String ts_code){
+	public String communityDetail(Model model, String ts_code) {
 		logger.info("SELECT ONE");
-		
+
 		model.addAttribute("community", biz.selectList_D());
 		model.addAttribute("detail", biz.selectOne_D(ts_code));
-		return "TwTCommunity/community_detail"; 
+		return "TwTCommunity/community_detail";
 	}
-	
-	
-	
 
 }
