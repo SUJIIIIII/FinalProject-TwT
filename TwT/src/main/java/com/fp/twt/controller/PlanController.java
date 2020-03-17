9f9faf42ab000e0684144152fb0ee6b16d1c7c23
bxxmi <56878724+bxxmi@users.noreply.github.com>
2020-03-17 10:15:27 +0900

    다시

---
+++ TwT/src/main/java/com/fp/twt/controller/PlanController.java
@@ -0,0 +1,221 @@
+package com.fp.twt.controller;
+
+import java.io.File;
+
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fp.twt.biz.PlanBiz;
+import com.fp.twt.vo.CityVo;
+import com.fp.twt.vo.TravelPointVo;
+import com.fp.twt.vo.TravelScheduleVo;
+
+
+
+// http://localhost:8090/twt/plan.do
+
+@Controller
+public class PlanController {
+	@Autowired
+	public PlanBiz biz;
+	
+	@Resource(name="uploadPath")
+	String uploadPath;
+	
+	//철환
+	
+	@RequestMapping("/selectCity.do")
+	public String selectCity(Model model, HttpServletRequest request) {
+		// 도시변경
+		System.out.println("selectCity");
+		// city 모든 정보
+		List<CityVo> allCity = biz.selectCityAll();
+
+		// 해당하는 도시에 대한 Spot 정보
+		String citycode = request.getParameter("citycode");
+		System.out.println("controller에서 citycode : " + citycode);
+		List<TravelPointVo> cityspot = biz.selectCitySpot(citycode);// 도시별 spot 불러오기s
+
+		// 선택된 city 정보
+		CityVo cityvo = biz.selectCityOne(citycode);
+
+/*		System.out.println("allspot : " + allSpot);
+		System.out.println("위도 : " + allSpot.get(0).gettp_Lati());
+		System.out.println("경도 : " + allSpot.get(0).gettp_Long());*/
+
+		model.addAttribute("allCity", allCity);
+		model.addAttribute("allSpot", cityspot);
+		model.addAttribute("citycode", citycode);
+		model.addAttribute("cityvo", cityvo);
+		
+		return "TwTPlan/plan_spot_box";
+	}
+	
+	
+	
+	
+	
+	//-------------------------------------------------------------------------------------------------------------
+	
+	//수지
+	@RequestMapping("/plan.do")
+	public String createSelect(Model model) {
+		// city 모든 정보
+		List<CityVo> res = biz.selectCityAll(); 
+		model.addAttribute("res",res);
+		return "TwTPlan/plan_create";
+	}
+	
+	@RequestMapping("planDetail.do")
+	public String planDetail(Model model, HttpServletRequest request) {
+		// Day1 추가해서 만들기
+		
+		
+		// city 모든 정보
+		List<CityVo> allCity = biz.selectCityAll();
+		
+		// spot 모든 정보
+		String citycode = request.getParameter("citycode");
+		System.out.println("controller에서 citycode : " + citycode);
+//		List<TravelPointVo> allSpot = biz.selectAllSpot();
+		List<TravelPointVo> cityspot = biz.selectCitySpot(citycode);// 도시별 spot 불러오기s
+		
+		// 선택된 city 정보
+		CityVo cityvo = biz.selectCityOne(citycode);
+		
+//		System.out.println("allspot : " + cityspot);
+		System.out.println("allcity : " + allCity);
+		System.out.println("위도 : " + cityspot.get(0).gettp_Lati());
+		System.out.println("경도 : " + cityspot.get(0).gettp_Long());
+		
+		model.addAttribute("allCity",allCity);
+		model.addAttribute("allSpot",cityspot);
+		model.addAttribute("citycode",citycode);
+		model.addAttribute("cityvo",cityvo);
+		
+		return "TwTPlan/plan_detail";
+	}
+	
+	@RequestMapping(value = "/insertPlan.do", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, String> insertPlan(HttpServletRequest request, HttpSession session,
+			@RequestBody Map<String, HashMap<String, Object>> param) throws ParseException {
+		
+		HashMap<String, Object> day_list = param.get("day_list"); // Day_List를 맵에 넣어줌
+		HashMap<String, Object> form_data = param.get("form_data"); // form Data를 맵에 넣어줌
+		
+		System.out.println("form_data : " + form_data);
+		System.out.println("day_list_obj : " + day_list);
+		
+		UUID uuid = UUID.randomUUID(); // 고유번호 만들어주기
+		String user_code = (String) session.getAttribute("userCode"); //유저번호 가져오기
+		String file_name = uuid.toString() + "_" + (String) form_data.get("file_name").toString();
+		String pn_title = (String)form_data.get("pn_title");
+		String pn_day = (String) form_data.get("pn_day").toString();
+		int pn_person = Integer.parseInt((String) form_data.get("pn_person").toString());
+		String pn_desc = (String) form_data.get("pn_desc").toString();
+		String pn_type = (String) form_data.get("pn_type").toString();
+		int pn_period = day_list.size();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH);
+		Date s_day = sdf.parse(pn_day);
+		
+		
+		TravelScheduleVo ts_vo = new TravelScheduleVo(); // vo에 정보 담아주기
+		ts_vo.setm_Code(user_code);
+		ts_vo.setts_Thum(file_name);
+		ts_vo.setts_Title(pn_title);
+		ts_vo.setts_People(pn_person);
+		ts_vo.setts_Theme(pn_type);
+		ts_vo.setts_Period(pn_period);
+		ts_vo.setts_Sday(s_day);
+				
+		int res = biz.insertSchedule(ts_vo);
+		
+		if(res == 0) {
+			System.out.println("insert가 실패하였습니다.");
+		} 
+		
+		String ts_code = "TS11" + ts_vo.getre_Code(); // TS_CODE 담아주기
+
+		
+		for (int i = 1; i < day_list.size(); i++) {
+			Object select_day = day_list.get("day"+i);
+			String select_day_str = select_day.toString();
+			
+			System.out.println("Day"+ i + " toStirng : " + select_day_str);
+			
+			select_day_str = select_day_str.replaceAll("\\{", "");
+			select_day_str = select_day_str.replaceAll("\\}", "");
+			
+			System.out.println("Day"+ i + " toStirng replace : " + select_day_str);
+			
+			String[] day_arr =  select_day_str.split(",");
+			for (int j = 0; j < day_arr.length; j++) {
+				String day_spot_num = day_arr[j].split("=")[1];
+				ts_vo.settp_Code(day_spot_num);
+				ts_vo.setTs_Day("DAY"+i);
+				
+				System.out.println("day_spot_num : " + day_spot_num);
+				
+				int res2 = biz.insertScheduleDay(ts_vo); 
+				
+				if(res2 == 0 ) {
+					System.out.println("insert가 실패하였습니다.");
+				}
+			}
+		}
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("file_name", uuid.toString());
+
+		return map;
+	}
+
+	@RequestMapping(value = "/fileUpload.do", method = RequestMethod.POST)
+	public String fileUpload(MultipartFile file, HttpServletRequest request) {
+		
+		// ------- file upload --------
+		System.out.println("fileName : " + file.getOriginalFilename());
+		String uuid = request.getParameter("file_name");
+		String real_name = uuid + "_" + file.getOriginalFilename();
+		
+		File target = new File(uploadPath, real_name);
+		try{
+			// 임시 디렉토리에 저장된 파일을 지정된 디렉토리로 복사
+			FileCopyUtils.copy(file.getBytes(), target);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return "redirect:/index.jsp";
+	}	
+
+	
+
+}
---
+++ TwT/src/main/java/com/fp/twt/dao/MypageDaoImpl.java
@@ -0,0 +1,357 @@
+package com.fp.twt.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.fp.twt.vo.AirplaneInfoVo;
+import com.fp.twt.vo.HotelBookingVo;
+import com.fp.twt.vo.HotelReviewVo;
+import com.fp.twt.vo.HotelVo;
+import com.fp.twt.vo.MemberVo;
+import com.fp.twt.vo.TravelScheduleVo;
+
+@Repository
+public class MypageDaoImpl implements MypageDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	// 회원가입
+	@Override
+	public int memberInsert(MemberVo vo) {
+		int res = 0;
+
+		try {
+			res = sqlSession.insert(namespace + "insertM", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 로그인
+	@Override
+	public MemberVo memberLogin(MemberVo vo) {
+		MemberVo res = null;
+
+		try {
+			res = sqlSession.selectOne(namespace + "loginM", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 아이디 중복검사
+	@Override
+	public int idChk(String m_Id) {
+		return sqlSession.selectOne(namespace + "idChkM", m_Id);
+	}
+
+	// 네이버 로그인
+	@Override
+	public MemberVo naverlogin(MemberVo vo) {
+		MemberVo res = null;
+
+		res = sqlSession.selectOne(namespace + "naverM", vo);
+
+		return res;
+	}
+
+	// 항공권 조회
+	@Override
+	public List<AirplaneInfoVo> selectAirList(String m_Code) {
+		List<AirplaneInfoVo> list = new ArrayList<AirplaneInfoVo>();
+
+		System.out.println("마이페이지 다오 임플 : " + m_Code);
+
+		try {
+			list = sqlSession.selectList(namespace + "selectAllAirM", m_Code);
+			for (AirplaneInfoVo i : list) {
+				System.out.println(i.toString());
+			}
+		} catch (Exception e) {
+			System.out.println("전체 항공권 리스트 에러");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 항공권 입력
+	@Override
+	public int insertAir(AirplaneInfoVo vo) {
+		int res = 0;
+
+		try {
+			res = sqlSession.insert(namespace + "insertAirM", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 유저 인증키 생성
+	@Override
+	public int GetKey(String m_Id, String key) {
+		System.out.println("다오 임플에 들어오는 아이디:" + m_Id);
+		System.out.println("다오 임플에 들어오는 키 : " + key);
+		return sqlSession.update(namespace + "GetKey", m_Id);
+	}
+
+	// 유저 인증키 Y 변경
+	@Override
+	public int alterUserKey(String m_Id, String key) {
+		System.out.println("다오 임플에 들어오는 아이디:" + m_Id);
+		System.out.println("다오 임플에 들어오는 키 : " + key);
+		return sqlSession.update(namespace + "alterUserKey", m_Id);
+	}
+
+	// 항공권 정보 삭제
+	@Override
+	public Object deleteAir(String air_Code) {
+		System.out.println("삭제버튼 누른 항공권 정보의 에어코드 : " + air_Code);
+		return sqlSession.delete(namespace + "deleteAirM", air_Code);
+	}
+
+	// 회원탈퇴
+	@Override
+	public Object deleteAccount(String m_Code) {
+		System.out.println("탈퇴버튼 누른 회원 코드 : " + m_Code);
+		return sqlSession.delete(namespace + "deleteUserM", m_Code);
+	}
+
+	// 항공권 정보 수정
+	@Override
+	public Object updateAir(AirplaneInfoVo vo) {
+		System.out.println("수정할 항공권 vo : " + vo.toString());
+		return sqlSession.update(namespace + "updateAirM", vo);
+	}
+
+	// 로그인시 디비에 저장된 아이디
+	@Override
+	public int loginIdChk(String m_Id) {
+		int res = 0;
+
+		try {
+			res = sqlSession.selectOne(namespace + "loginIdChk", m_Id);
+			
+			if(res > 0) {
+				System.out.println("아이디 존재");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 로그인시 디비에 저장된 비번확인
+	@Override
+	public int loginPwdChk(String m_Pass) {
+		int res = 0;
+
+		try {
+			res = sqlSession.selectOne(namespace + "loginPwdChk", m_Pass);
+			
+			if(res > 0) {
+				System.out.println("비밀번호 존재");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 로그인시 디비에 저장된 모든 정보 검토
+	@Override
+	public MemberVo selectOneLogin(MemberVo vo) {
+		MemberVo res = sqlSession.selectOne(namespace + "loginSelectOne", vo);
+		return res;
+	}
+
+	// 비밀번호 변경
+	@Override
+	public void memberUpdate(MemberVo vo) {
+		sqlSession.update(namespace + "updateM", vo);
+	}
+
+	// 비밀번호 찾기
+	@Override
+	public void searchPassword(MemberVo vo) {
+		// TODO Auto-generated method stub
+		sqlSession.update(namespace + "searchPasswordM", vo);
+	}
+
+	// 호텔 예약 조회
+	@Override
+	public List<HotelBookingVo> selectAllHotelBooking(String m_Code) {
+		List<HotelBookingVo> list = new ArrayList<HotelBookingVo>();
+
+		System.out.println("호텔 예약 조회할 멤버 번호 : " + m_Code);
+
+		try {
+			list = sqlSession.selectList(namespace + "selectAllBookM", m_Code);
+			for (HotelBookingVo i : list) {
+				System.out.println(i.toString());
+			}
+		} catch (Exception e) {
+			System.out.println("전체 호텔 예약 리스트 에러");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// SNS로그인 시 자동 회원가입
+	@Override
+	public int memberSNSInsert(MemberVo vo) {
+		int res = 0;
+
+		try {
+			res = sqlSession.insert(namespace + "insertSNSM", vo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 찜 목록 조회
+	@Override
+	public List<TravelScheduleVo> selectAllLike(String m_Code) {
+		List<TravelScheduleVo> list = new ArrayList<TravelScheduleVo>();
+
+		try {
+			list = sqlSession.selectList(namespace + "likeM", m_Code);
+			for (TravelScheduleVo i : list) {
+				System.out.println(i.toString());
+			}
+		} catch (Exception e) {
+			System.out.println("찜목록 조회 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 내가 쓴 일정 조회
+	@Override
+	public List<TravelScheduleVo> selectMyRoute(String m_Code) {
+		List<TravelScheduleVo> list = new ArrayList<TravelScheduleVo>();
+
+		try {
+			list = sqlSession.selectList(namespace + "routeM", m_Code);
+			for (TravelScheduleVo i : list) {
+				System.out.println(i.toString());
+			}
+		} catch (Exception e) {
+			System.out.println("내 일정 조회 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 전체 회원 조회
+	@Override
+	public List<MemberVo> selectAllMember(MemberVo vo) {
+		List<MemberVo> list = new ArrayList<MemberVo>();
+		try {
+			list = sqlSession.selectList(namespace + "allmemberM", vo);
+			for (MemberVo i : list) {
+				System.out.println(i.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 아이디 찾기
+	@Override
+	public List<MemberVo> searchId(String m_Name, String m_Email) {
+		List<MemberVo> list = new ArrayList<MemberVo>();
+		try {
+			list = sqlSession.selectList(namespace + "findIdM", m_Email);
+		} catch (Exception e) {
+			System.out.println("아이디 찾기 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 예약된 호텔 조회
+	@Override
+	public List<HotelVo> selectOneHotel(String m_Code) {
+		List<HotelVo> list = new ArrayList<HotelVo>();
+
+		try {
+
+			list = sqlSession.selectList(namespace + "hotelM", m_Code);
+
+		} catch (Exception e) {
+			System.out.println("예약된 호텔의 이름 가져오기 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 별점부여
+	@Override
+	public int insertStar(HotelReviewVo vo) {
+		int res = 0;
+
+		try {
+			res = sqlSession.insert(namespace + "insertStarM", vo);
+			System.out.println("res : " + res);
+		} catch (Exception e) {
+			System.out.println("별점 부여 실패");
+			e.printStackTrace();
+		}
+		return res;
+	}
+
+	// 별점 조회
+	@Override
+	public List<HotelReviewVo> selectRating(String m_Code) {
+		List<HotelReviewVo> list = new ArrayList<HotelReviewVo>();
+
+		try {
+			list = sqlSession.selectList(namespace + "selectStarM", m_Code);
+			for (HotelReviewVo i : list) {
+				System.out.println(m_Code + "가 부여한 별점 : " + i.getHrv_Starn());
+			}
+		} catch (Exception e) {
+			System.out.println("별점 조회 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 업뎃할 호텔 별점 조회
+	@Override
+	public HotelVo selectOneHotelStar(String h_Code) {
+		HotelVo list = new HotelVo();
+
+		try {
+			list = sqlSession.selectOne(namespace + "selectHotelStarM", h_Code);
+			System.out.println(h_Code + "의 별점 : " + list.getH_Starn());
+		} catch (Exception e) {
+			System.out.println("호텔별 별점 조회 실패");
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 별점 업뎃
+	@Override
+	public Object updateStar(String h_Code, int point) {
+		System.out.println("다오임플 : " + point + "/" + h_Code);
+		Map<String, Object> key = new HashMap<String, Object>();
+		key.put("h_Code", h_Code);
+		key.put("star", point);
+
+		return sqlSession.update(namespace + "updateStarM", key);
+	}
+}
