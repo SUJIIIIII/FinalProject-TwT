e261303bf0acf47203ef1f4b462b1abda9b65825
DoYoung93 <qed4430@naver.com>
2020-03-19 13:05:07 +0900

    20.03.19 DELETE2

--- TwT/src/main/webapp/resources/js/chat.js
+++
@@ -1,5 +0,0 @@
-var chat = document.getElementById('chat');
-   chat.scrollTop = chat.scrollHeight - chat.clientHeight;
-   
-var chatlist = document.getElementById('chatlist');
-chatlist.scrollTop = chatlist.scrollHeight - chatlist.clientHeight;
